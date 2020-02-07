4832dc7433ac5ae1e413aee60a12dd50cc57c3dc
Conor Toomb <conor.toomb114@gmail.com>
2020-02-06 20:12:47 -0500

    added questions

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Node-Homework
-Used npm init and installed inquirer to add modules and json
+
+  # ctoomb1
+  ## Info
+  Email: [conor.toomb114@gmail.com](mailto:conor.toomb114@gmail.com)
+  ## Description
+  This
+  ## Installation
+  Download it
+  ## Usage
+  Easily
+  ## License
+  MIT
+  
---
+++ index.js
@@ -0,0 +1,62 @@
+// const axios = require('axios');
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateMarkdown = readMeData => {
+  return `
+  # ${readMeData.username}
+  ## Info
+  Email: [${readMeData.email}](mailto:${readMeData.email})
+  ## Description
+  ${readMeData.description}
+  ## Installation
+  ${readMeData.installation}
+  ## Usage
+  ${readMeData.usage}
+  ## License
+  ${readMeData.license}
+  `;
+};
+
+inquirer
+  .prompt([{
+    type: 'input',
+    message: 'What is your Github Username?',
+    name: 'username'
+  },
+  {
+    message: 'What is your email?',
+    type: 'input',
+    name: 'email'
+  },
+  {
+    message: 'Describe your project:',
+    type: 'input',
+    name: 'description'
+  },
+  {
+    message: 'How does the user install the app?',
+    type: 'input',
+    name: 'installation'
+  },
+  {
+    message: 'How does the user use the app?',
+    type: 'input',
+    name: 'usage'
+  },
+  {
+    message: 'What licenses did you use?',
+    choices: ['Apache License 2.0', 'GNU GPLv3', 'MIT', 'ISC'],
+    type: 'list',
+    name: 'license'
+  }]).then(responseObj => {
+    console.log(responseObj);
+    const finishedMarkdown = generateMarkdown(responseObj);
+
+    fs.writeFile('./readme.md', finishedMarkdown, err => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log('Success!');
+    });
+      });
